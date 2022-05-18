ef55e9f0c5024009b2c4c229d163079a284c4b70
ziclude <cindyzickziemi@gmail.com>
2022-05-18 15:16:36 +0100

    authors retouched

--- generate-authors.sh
+++ generate-authors.sh
@@ -4,9 +4,7 @@ set -e
 {
 	cat <<-'EOH'
 	# This file lists all individuals having contributed content to the repository.
-	Ana Cecilia Zapata Salas <https://github.com/ceciisalas>
-	Cindy Ziemi <https://github.com/ziclude>
-
+	
 	EOH
 	echo
 	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf | grep -v "github.com"
